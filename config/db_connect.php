192b16e30144da08d57cd80ec0e2bfcad12a5b47
AdamsChristopher <neanderthalnoise@gmail>
2019-03-07 15:35:51 +0100

    Modified db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,6 +2,9 @@
   $connection = mysqli_connect('localhost', 'user', '1234', 'phpizza');
   
   if (!$connection) {
-    echo 'Connection error: ' . mysqli_connect_error();
+    echo "Error: Unable to connect to MySQL: " . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
   }
 ?>
